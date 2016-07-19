9becc6a4b0ab27ce5ab0fcab225c7c5b37b74699
lior.shtivelman <lior.shtivelman@vexigo.com>
2016-07-19 18:33:46 +0300

    added parameters via command line to support url and aggragated or not type

--- test.js
+++ test.js
@@ -1,32 +1,75 @@
+// initial settings
+//---------------------------------------------------------------------------
 var webdriver = require('selenium-webdriver');
 
 var By        = webdriver.By;
 var until     = webdriver.until;
+var chrome    = require('../node_modules/selenium-webdriver/chrome');
 
-var userAgent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36";
+var userName  = "lior.shtivelman@vexigo.com";
+var accessKey = "69409b40-ca42-4cea-b9ca-2b16d7c3fcbe";
+
+// var ua        = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.23 Mobile Safari/537.36";
 
 var driver    = new webdriver.Builder()
-				.withCapabilities({'userAgent' : userAgent})
-				.forBrowser('chrome', 'ANDROID', 'ANDROID')
-				.build();
+				 .setChromeOptions(new chrome.Options()
+				 .setMobileEmulation({deviceName: 'Google Nexus 5'}))
+				 //.withCapabilities({ 'userame' : userName, 'accessKey': accessKey})
+				 //.usingServer('http://' + userName + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+				 .forBrowser('chrome')
+				 .build();
 
 
-var Color = require("colors");
-// var red = Color(255, 255, 255);
-// var green = Color(0, 255, 0);
-// var yellow = Color(255, 255, 0);
+var Color     = require("colors");
 
-var signs = {
-	ok :  "\u2714",
-	not_ok : "\u2716"
+//** 
+// main object
+//   with methods
+//*	
+var mainObj = {
+	// params
+	testUrl : 'http://vis.startscreen.com/news.yahoo.com',
+	isAggregated : false,
+	tmpVar : "",
+	signs : {
+		ok :  "\u2714",
+		not_ok : "\u2716"
+	},
+	textToCompare : "",
+
+	// functions
+	//-----------------------------------------------------------
+	clearIntervalAndCloseBrowser : fnClearIntervalAndCloseBrowser,
+	openCategories : fnOpenCategories,
+	runScript : fnRunScript,
+	checkIfWasRedirectedCorrectly : fnCheckIfWasRedirectedCorrectly,
+	openFirstFeed : fnOpenFirstFeed,
+	onArticleLoaded:  fnOnArticleLoaded,
+	checkValidityOfArticle : fnCheckValidityOfArticle,
+	isAggregatedUrl : fnIsAggregated,
+	runOnAggregatedFeed : fnRunOnAggregatedFeed,
+	runOnNonAggregatedFeed : fnRunOnNonAggregatedFeed
 }
 
-// var visualizrUrl  = 'http://vis.startscreen.com/trending';
-var visualizrUrl  = 'http://vis.startscreen.com/news.yahoo.com';
+// start tests
+console.log("****** starting monitoring tests *******");
+
+if (process.argv.length > 0){
+	var args = process.argv.slice(2);
+
+	args.forEach((arg, index) => {
+		if (arg.indexOf("url=") > -1){
+			 mainObj.testUrl = arg.substring(arg.indexOf("=") + 1);
+		}
+
+		if (arg.indexOf("is_aggregated") > -1){
+			mainObj.isAggregated = mainObj.isAggregatedUrl(arg.split("=")[1]);
+		}
+	});
+}
 
 
-driver.get(visualizrUrl);
-driver.switchTo().alert().accept();
+driver.get(mainObj.testUrl);
 
 setTimeout(function () {
 	var counter = 0;
@@ -43,97 +86,100 @@ setTimeout(function () {
 				});
 
 		counter++;				
-		if (counter == 5){
-			clearIntervalAndCloseBrowser(interval, driver);
-			var str = ` * number of articels = ${articles.length} ${signs.ok}`;
+		if (counter == 5) {
+			mainObj.clearIntervalAndCloseBrowser(interval, driver);
+			var str = ` * number of articels = ${articles.length} ${mainObj.signs.ok}`;
 			console.log(str.green);
-			// openCategories();
-			runScript();
-			//driver.quit();
+			mainObj.runScript();
 		}
 	}, 1000);
 }, 5000);
 
-function clearIntervalAndCloseBrowser(interval, browserObj) {
+/// functions
+/// =====================================
+
+function fnClearIntervalAndCloseBrowser(interval, browserObj) {
 	clearInterval(interval);
-	// browserObj.quit();
 }
 
 
-function openCategories(){
+function fnOpenCategories(){
 	driver.executeScript('document.querySelector(".vex-icon-menu").click()');
 }
 
-function runScript(){
+function fnRunScript(){
 	var categoryText = "";
 	driver.executeScript('return document.querySelector("nav#menuleft ul")').then(
 		function(result) {
 			if (result == null){
-				var msg = ` * Categories menu is missing! ${signs.not_ok}`;
+				var msg = ` * Categories menu is missing! ${mainObj.signs.not_ok}`;
 				console.log(msg.red);
 			}
 			else{
 				driver.executeScript('return document.querySelector("#menuleft ul").children.length').then(function(result) {					
 					if (result == 1){
-						console.error(` * No items in categories menu!' ${signs.not_ok}`.red);
+						console.error(` * No items in categories menu!' ${mainObj.signs.not_ok}`.red);
 					}
 					else{
-						//driver.findElement(By.className("vex-icon-menu")).click().then(
-						driver.findElement(By.xpath('//*[@id="vis-top-bar"]/ul/li[1]/a/span[1]'))
-							.click()
-							.then(
-								function() {
-									var textElementToCompare = "";
-									var msg = ` * There are ${result} items in categories menu ${signs.ok}`;
-									console.log(msg.green);	
-									console.log(' * Simulating clicking on first category.....');
+						var aggregatedScript = 'return document.querySelectorAll(".child-cat-container")[1]';
+						var nonAggregatedPath = '//*[@id="vis-top-bar"]/ul/li[1]/a/span[1]';
+						var scriptToRun = mainObj.isAggregated ?  aggregatedScript : nonAggregatedPath;
 
 
-									setTimeout(function() {
-										// get text of first category
-										driver.executeScript('return document.querySelectorAll(".vis-master-cat")[1].innerText')
-											.then(
-													function(text) {
-														// console.log("text to compare is " + text);
-														textElementToCompare = text;
-
-														driver.findElements(By.className('vis-master-cat'))
-															.then(
-																	function(webElementsArray) {
-																		webElementsArray[1]
-																			.click()
-																			.then(function() {
-																				setTimeout(
-																					function() {
-																						checkIfWasRedirectedCorrectly(textElementToCompare);
-																					},
-												 									3000);
-																			});
-																	},
-
-																	function(err) {
-																		console.log("something is wrong again....", err);
-																	}
-															);
-													},
-
-													function(err) {
-														console.error("something is wrong!", err);
-													}
-												);
-									}, 2000);
+						if (mainObj.isAggregated){
+							mainObj.runOnAggregatedFeed();
+						}
+						else{
+								mainObj.runOnNonAggregatedFeed();
+						}
 
 
-									
-										// .click()
-										// .then(function(item) {
-										// 	setTimeout(function() {
-										// 		console.log(' * redirecting.... ');
+								// 		// click goes here
 
-										
-										// 	}, 5000);	
-										// });
-							});
+								// 		return;
+								// 		var textElementToCompare = "";
+								// 		var msg = ` * There are ${result} items in categories menu ${mainObj.signs.ok}`;
+								// 		console.log(msg.green);	
+								// 		console.log(' ===> Simulating clicking on first category...');
+
+								// 		console.log(" * aggregated = " + mainObj.isAggregated);
+
+								// 		// setTimeout(function() {
+								// 		// 	// get text of first category
+								// 		// 	driver.executeScript('return document.querySelectorAll(".vis-master-cat")[1].innerText')
+								// 		// 		.then(
+								// 		// 				function(text) {
+								// 		// 					// console.log("text to compare is " + text);
+								// 		// 					textElementToCompare = text;
+
+								// 		// 					driver.findElements(By.className('vis-master-cat'))
+								// 		// 						.then(
+								// 		// 								function(webElementsArray) {
+								// 		// 									webElementsArray[1]
+								// 		// 										.click()
+								// 		// 										.then(function() {
+								// 		// 											setTimeout(
+								// 		// 												function() {
+								// 		// 													mainObj.checkIfWasRedirectedCorrectly(textElementToCompare);
+								// 		// 												},
+								// 		// 			 									3000);
+								// 		// 										});
+								// 		// 								},
+
+								// 		// 								function(err) {
+								// 		// 									console.log("something is wrong again....", err);
+								// 		// 								}
+								// 		// 						);
+								// 		// 				},
+
+								// 		// 				function(err) {
+								// 		// 					console.error("something is wrong!", err);
+								// 		// 				}
+								// 		// 			);
+								// 		// }, 2000);
+								// });
+
+							// })
 					}	
 				});
 			}
@@ -144,40 +190,27 @@ function runScript(){
 	)
 }
 
-
-// function getMenuElement() {
-// 	driver.executeScript("")
-// }
-
-function checkIfWasRedirectedCorrectly(textToCompare) {
+function fnCheckIfWasRedirectedCorrectly(textToCompare) {
 	setTimeout(
 		function() {
-			// driver.executeScript('document.querySelector("#menuleft ul").children[1].children[1].children[1].children[1].children[0].children[0].click()').then(function(response) {
-			// 	//console.log(" * Response is " + response);
-			// });	
-
-			// console.log(' * Waiting for page to redirect....');
 			setTimeout(function() {
-				//driver.executeScript('return window.location.pathname.split("/")').then(function(result) {}, 1500);
 				driver.executeScript('return document.querySelector("#vis-top-bar ul").children[1].children[0].innerText').then(
 					function(response) {
 						var msg = "";
 						if (response == textToCompare){
-							console.log(` * page has redirected correctly ${signs.ok}`.green);
-							openFirstFeed();
+							console.log(` * page has redirected correctly ${mainObj.signs.ok}`.green);
+							mainObj.openFirstFeed();
 						}
 						else{
-							console.error(` !!! page did not redirect correctly !!! ${signs.not_ok}`.red);
+							console.error(` !!! page did not redirect correctly !!! ${mainObj.signs.not_ok}`.red);
 						}
-
-						//driver.quit();
 					}
 				);
 			}, 4000);	
 	}, 2000);
 }
 
-function openFirstFeed() {
+function fnOpenFirstFeed() {
 	setTimeout(function() {
 		driver.executeScript(`
 				document.querySelector(".vexMagazineOverlay").style.display = "none";
@@ -187,45 +220,14 @@ function openFirstFeed() {
 				`)
 					.then(function() {
 						setTimeout(function() {
-							onArticleLoaded();
+							mainObj.onArticleLoaded();
 						}, 5000);	
 					});
 	}, 1000 * 9);	
 }
 
-
-
-function onArticleLoaded() {
-	// driver.findElements(By.className('article-banner'))
-	// 	.then(function(elmArr) {
-	// 		console.log(` * There are ${elmArr.length} banner containers on page`);
-
-	// 		// var scriptToExecute = 'var banners = document.querySelectorAll(".article-banner");' + 
-	// 		// 'console.log(banners);\n' + 
-	// 		// 'if (banners.length > 0){\n' + 
-	// 		// 	'banners.forEach(banner => {\n' + 
-	// 		// 		'if (banner.children.length){\n' + 
-	// 		// 			'var innerItems = banner.children[0].children;\n' +
-	// 		// 			'console.log(innerItems);\n' +
-						
-	// 		// 		'}\n' + 
-	// 		// 		'else{\n' + 
-	// 		// 			'var str = "banner with id " + banner.getAttribute("id") + " has no children!";\n' + 
-	// 		// 			'console.log(str.red);\n' + 
-	// 		// 		'}\n' +
-	// 		// 	'});\n' +
-	// 		// '};';
-
-	// 		// driver.executeScript(scriptToExecute)
-	// 		// 	.then(function(response) {
-	// 		// 		console.log("response ->", response);
-	// 		// 	});
-	// 	})
-	// 	
-	// 	
-	// 	
-	// 	
-	var script = `	var banners = document.querySelectorAll(".article-banner");
+function fnOnArticleLoaded() {
+	var script = `	var banners = document.querySelectorAll(".article-banner-place-holder");
 				  	var notLoadedCounter = 0;
 			  	  	var loadedCounter = 0;
 					for (var i=0; i<banners.length; i++) {
@@ -249,22 +251,22 @@ function onArticleLoaded() {
 			.then(function(res) {
 				var msg;
 				if (res.loadedCounter == res.totalBanners){
-					msg = ` * all banners were loaded on page ${signs.ok}`;
+					msg = ` * all banners were loaded on page ${mainObj.signs.ok}`;
 					console.log(msg.green);
 				}
 				else {
-					msg = ' * not all banners were loaded. ${res.loadedCounter} out of ${res.totalBanners} were loaded ${signs.not_ok}';
+					msg = ` * not all banners were loaded. ${res.loadedCounter} out of ${res.totalBanners} were loaded ${mainObj.signs.not_ok}`;
 					console.log(msg.red);
 				}
 
-				checkValidityOfArticle(); 	
+				mainObj.checkValidityOfArticle(); 	
 				
 			});		
 	}, 1000 * 10);				
 }
 
 
-function checkValidityOfArticle() {
+function fnCheckValidityOfArticle() {
 	var script = 'var paragraphs = document.getElementsByTagName("p");' + 
 			     'var emptyCounter = 0;' +
 				 'for (var i=0; i<paragraphs.length; i++) {' +
@@ -281,11 +283,11 @@ function checkValidityOfArticle() {
 			fullPercentage = parseFloat(fullPercentage.toFixed(2));
 			
 			if (fullPercentage >= 50){
-				var msg = ` * Article is loaded...${fullPercentage}% of paragraphs have text in them ${signs.ok}`;
+				var msg = ` * Article is loaded...${fullPercentage}% of paragraphs have text in them ${mainObj.signs.ok}`;
 				console.log(msg.green);
 			}
 			else{
-				var err = ` * less then 50% of the paragraphs have text in them. ${signs.not_ok}`;
+				var err = ` * less then 50% of the paragraphs have text in them. ${mainObj.signs.not_ok}`;
 				console.error(err.red);
 			}
 		});
@@ -310,15 +312,79 @@ function checkValidityOfArticle() {
 		.then(function(response) {
 			var msg = "";
 			if (response.loaded == response.totalNumberOfElements){
-				msg = ` * All social elements were loaded. ${signs.ok}`;
+				msg = ` * All social elements were loaded. ${mainObj.signs.ok}`;
 				console.log(msg.green);
 			}
 			else {
 				msg = ` * Loaded ${response.loaded} of ${response.totalNumberOfElements} social elements. ${signs.not_ok}`; 
 				console.log(msg.red);
 			}
+
+			driver.quit();
 		});
 }
 
+function fnIsAggregated(value) {
+	if (typeof(value) != 'undefined'){
+		return (value == 1 || value == '1') ? true : false;
+	}
+	else{
+		return false;
+	}
+}
+
+function fnRunOnAggregatedFeed() {
+	driver.findElements(By.className("left-symbol"))
+		.then(function(items) {
+			items[0]
+				.click()
+				.then(function() {
+					driver.executeScript(`document.querySelector("#menuleft").children[1].children[1].children[0].click()`)
+						.then(function(items) {
+							setTimeout(function() {
+									driver.executeScript('return document.querySelector("#first-cat > a").innerText')
+										.then(function(result) {
+											mainObj.textToCompare = result;
+											driver.executeScript('document.querySelector("#first-cat > a").click()')
+												.then(function() {
+													setTimeout(function() {
+														mainObj.checkIfWasRedirectedCorrectly(mainObj.textToCompare);
+													}, 3000);
+												});
+
+										})
+										
+							}, 1000);
+						});
+				});
+		});
+}
+
+function fnRunOnNonAggregatedFeed() {
+	driver.findElements(By.className("left-symbol"))
+		.then(function(items) {
+			items[0]
+				.click()
+				.then(function() {
+					driver.executeScript(`document.querySelector("#menuleft").children[1].children[1].children[0].click()`)
+						.then(function(items) {
+							setTimeout(function() {
+									driver.executeScript('return document.querySelector(".vis-master-cat").innerText')
+										.then(function(result) {
+											mainObj.textToCompare = result;
+											driver.executeScript('document.querySelector(".vis-master-cat").click()')
+												.then(function() {
+													setTimeout(function() {
+														mainObj.checkIfWasRedirectedCorrectly(mainObj.textToCompare);
+													}, 3000);
+												});
+
+										})
+										
+							}, 1000);
+						});
+				});
+		});
+}
 
 
